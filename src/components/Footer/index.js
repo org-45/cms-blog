8d65bb7372f444383f553f1c6b8c4b1f92612535
Krishna <7krishna7dhakal7@gmail.com>
2021-05-15 13:07:38 +0545

    footer updates

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
-import { Field } from "formik";
+// import { Field } from "formik";
+import FooterNavbar from "../FooterNav";
 
 const Footer = (props) => {
   const { copyright } = props;
 
   return (
-    <footer className="flex flex-col bg-gray-900 h-90vh">
+    <footer className="flex flex-col bg-gray-900 h-80vh text-white">
       <div className="bg-black h-15vh grid justify-center items-center">
         <StaticImage
           src="../../assets/images/logo/logo1.png"
@@ -17,7 +18,7 @@ const Footer = (props) => {
           height={100}
         />
       </div>
-      <div className="bg-black h-5vh flex flex-row justify-center items-center text-white">
+      <div className="bg-black h-5vh grid grid-cols-1 justify-center items-center text-white">
         <div className="grid justify-center items-center text-white px-5">
           {`Get updates from us`}
         </div>
@@ -32,24 +33,84 @@ const Footer = (props) => {
           </form>
         </div>
       </div>
-      <div className="bg-yellow-600 h-30vh grid justify-center items-center">
-        Main
+      <div className="bg-black h-30vh grid justify-center items-center">
+        <div className="grid grid-cols-5 gap-x-20 ">
+          <div className="grid grid-cols-1">
+            <div className=" grid justify-center items-center p-3 font-bold">
+              PRODUCTS
+            </div>
+            <div className=" grid justify-center items-center">0Chain Net</div>
+            <div className=" grid justify-center items-center">0Box</div>
+            <div className=" grid justify-center items-center">0Wallet</div>
+            <div className=" grid justify-center items-center">0Lake</div>
+            <div className=" grid justify-center items-center">0Stor</div>
+          </div>
+          <div className="grid grid-cols-1">
+            <div className=" grid justify-center items-center p-3 font-bold">
+              PRODUCTS
+            </div>
+            <div className=" grid justify-center items-center">0Chain Net</div>
+            <div className=" grid justify-center items-center">0Box</div>
+            <div className=" grid justify-center items-center">0Wallet</div>
+            <div className=" grid justify-center items-center">0Lake</div>
+            <div className=" grid justify-center items-center">0Stor</div>
+          </div>
+          <div className="grid grid-cols-1">
+            <div className=" grid justify-center items-center p-3 font-bold">
+              PRODUCTS
+            </div>
+            <div className=" grid justify-center items-center">0Chain Net</div>
+            <div className=" grid justify-center items-center">0Box</div>
+            <div className=" grid justify-center items-center">0Wallet</div>
+            <div className=" grid justify-center items-center">0Lake</div>
+            <div className=" grid justify-center items-center">0Stor</div>
+          </div>
+          <div className="grid grid-cols-1">
+            <div className=" grid justify-center items-center p-3 font-bold">
+              PRODUCTS
+            </div>
+            <div className=" grid justify-center items-center">0Chain Net</div>
+            <div className=" grid justify-center items-center">0Box</div>
+            <div className=" grid justify-center items-center">0Wallet</div>
+            <div className=" grid justify-center items-center">0Lake</div>
+            <div className=" grid justify-center items-center">0Stor</div>
+          </div>
+          <div className="grid grid-cols-1">
+            <div className=" grid justify-center items-center">
+              <button className="px-2" style={{ border: "1px solid white" }}>
+                Try Betanet
+              </button>
+              <button className="px-2" style={{ border: "1px solid white" }}>
+                Try App
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="bg-green-600 h-10vh grid justify-center items-center">
-        Socials
+      <div className="bg-black h-5vh grid justify-center items-center">
+        <div className="grid grid-cols-8 gap-x-10">
+          <div className="grid">facebook</div>
+          <div className="grid">youtube</div>
+          <div className="grid">3</div>
+          <div className="grid">4</div>
+          <div className="grid">5</div>
+          <div className="grid">3</div>
+          <div className="grid">4</div>
+          <div className="grid">5</div>
+        </div>
       </div>
-      <div className="bg-blue-600 h-10vh grid justify-center items-center">
+      <div className="bg-black h-10vh grid justify-center items-center">
         <p>{copyright}</p>
-        <p>
-          <a href="https://github.com/0chain">0chain</a>
-        </p>
-        t
       </div>
-      <div className="bg-gray-600 h-10vh grid justify-center items-center">
-        Blog Dropdowns
+      <div className="bg-gray-1600 h-10vh grid justify-center items-center">
+        <FooterNavbar />
       </div>
-      <div className="bg-yellow-400 h-5vh grid justify-center items-center">
-        <div className="">Terms</div>
+      <div className="bg-black h-5vh grid justify-center items-center">
+        <div className="flex flex-row gap-x-5">
+          <div>Terms</div>
+          <div>GDPA/CCPA</div>
+          <div>Policy</div>
+        </div>
       </div>
     </footer>
   );
