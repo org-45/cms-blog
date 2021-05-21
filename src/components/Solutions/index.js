de132f89ac94efdfca33d10329ddfbb0104eee79
Krishna <7krishna7dhakal7@gmail.com>
2021-05-21 16:32:40 +0545

    solutions sections

--- src/components/Solutions/index.js
+++ src/components/Solutions/index.js
@@ -3,160 +3,284 @@ import React from "react";
 import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
-export default function Solutions() {
+//blockchain
+import BlockChainSolution from "/static/img/gfx/blockchain_integration.png";
+import BCS1 from "/static/img/gfx/msp/blockchain/0box-Icons_protect-original-content.svg";
+import BCS2 from "/static/img/gfx/msp/blockchain/0chain_Icons_privacy-data-compliance.svg";
+import BCS3 from "/static/img/gfx/msp/blockchain/0chain_Icons_fast-easy-record-transfer.svg";
+
+//social media
+import SocialMediaSolution from "/static/img/gfx/msp/0chain-privacy.png";
+import SMS1 from "/static/img/gfx/msp/blockchain/msps_liability.svg";
+import SMS2 from "/static/img/gfx/msp/blockchain/0chain_Icons_privacy-data-compliance.svg";
+import SMS3 from "/static/img/gfx/msp/blockchain/0chain Icons_protect-brand.svg";
+
+//health care
+import HealthCareSolution from "/static/img/gfx/msp/0chain-healthcare.png";
+import HCS1 from "/static/img/gfx/msp/blockchain/0box-Icons_protect-original-content.svg";
+import HCS2 from "/static/img/gfx/msp/blockchain/0chain_Icons_privacy-data-compliance.svg";
+import HCS3 from "/static/img/gfx/msp/blockchain/0chain_Icons_fast-easy-record-transfer.svg";
+
+//Financials
+import FinancialSolution from "/static/img/gfx/msp/0chain-identity.png";
+import FS1 from "/static/img/gfx/msp/blockchain/0chain Icons_prevent-fraud.svg";
+import FS2 from "/static/img/gfx/msp/blockchain/0chain_Icons_privacy-data-compliance.svg";
+import FS3 from "/static/img/gfx/msp/blockchain/0chain Icons_secured-sharing.svg";
+
+//supply chain
+import SupplyChainSolution from "/static/img/gfx/msp/0chain-supplychain.png";
+import SCS1 from "/static/img/gfx/msp/blockchain/0chain Icons_protect-brand.svg";
+import SCS2 from "/static/img/gfx/msp/blockchain/msps_liability.svg";
+import SCS3 from "/static/img/gfx/msp/blockchain/0chain Icons_prevent-fraud.svg";
+
+const Solutions = () => (
+  <div className="grid justify-center w-full ">
+    <Tabs className="w-full">
+      <TabList className="">
+        <Tab>BlockChain</Tab>
+        <Tab>Social Media</Tab>
+        <Tab>Supply Chain</Tab>
+        <Tab>Financials</Tab>
+        <Tab>Health Care</Tab>
+      </TabList>
+      <TabPanel>
+        <BlockChain />
+      </TabPanel>
+      <TabPanel>
+        <SocialMedia />
+      </TabPanel>
+      <TabPanel>
+        <SupplyChain />
+      </TabPanel>
+      <TabPanel>
+        <Financials />
+      </TabPanel>
+      <TabPanel>
+        <HealthCare />
+      </TabPanel>
+    </Tabs>
+  </div>
+);
+
+export default Solutions;
+
+export const BlockChain = () => {
   return (
-    <div dir="rtl">
-      <Tabs direction={"rtl"}>
-        <TabList>
-          <Tab>כותרת</Tab>
-          <Tab disabled>כותרת כבויה</Tab>
-          <Tab>כותרת שנייה</Tab>
-          <Tab>כותרת שלישית</Tab>
-        </TabList>
-        <TabPanel>
-          <p>
-            לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית לורם איפסום
-            דולור סיט אמט, קונסקטורר אדיפיסינג אלית. סת אלמנקום ניסי נון ניבאה.
-            דס איאקוליס וולופטה דיאם. וסטיבולום אט דולור, קראס אגת לקטוס וואל
-            אאוגו וסטיבולום סוליסי טידום בעליק. קונדימנטום קורוס בליקרה, נונסטי
-            קלובר בריקנה סטום, לפריקך תצטריק לרטי.
-          </p>
-          <p>
-            Source:{" "}
-            <a href="https://en.wikipedia.org/wiki/" target="_blank">
-              Wikipedia
-            </a>
-          </p>
-        </TabPanel>
-        <TabPanel>
-          <p>
-            לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית לורם איפסום
-            דולור סיט אמט, קונסקטורר אדיפיסינג אלית. סת אלמנקום ניסי נון ניבאה.
-            דס איאקוליס וולופטה דיאם. וסטיבולום אט דולור, קראס אגת לקטוס וואל
-            אאוגו וסטיבולום סוליסי טידום בעליק. קונדימנטום קורוס בליקרה, נונסטי
-            קלובר בריקנה סטום, לפריקך תצטריק לרטי.
-          </p>
-          <p>
-            Source:{" "}
-            <a href="https://en.wikipedia.org/wiki/" target="_blank">
-              Wikipedia
-            </a>
-          </p>
-        </TabPanel>
-        <TabPanel>
-          <p>
-            לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית לורם איפסום
-            דולור סיט אמט, קונסקטורר אדיפיסינג אלית. סת אלמנקום ניסי נון ניבאה.
-            דס איאקוליס וולופטה דיאם. וסטיבולום אט דולור, קראס אגת לקטוס וואל
-            אאוגו וסטיבולום סוליסי טידום בעליק. קונדימנטום קורוס בליקרה, נונסטי
-            קלובר בריקנה סטום, לפריקך תצטריק לרטי. לורם איפסום דולור סיט אמט,
-            קונסקטורר אדיפיסינג אלית לורם איפסום דולור סיט אמט, קונסקטורר
-            אדיפיסינג אלית. סת אלמנקום ניסי נון ניבאה. דס איאקוליס וולופטה דיאם.
-            וסטיבולום אט דולור, קראס אגת לקטוס וואל אאוגו וסטיבולום סוליסי טידום
-            בעליק. קונדימנטום קורוס בליקרה, נונסטי קלובר בריקנה סטום, לפריקך
-            תצטריק לרטי. לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית
-            לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית. סת אלמנקום ניסי
-            נון ניבאה. דס איאקוליס וולופטה דיאם. וסטיבולום אט דולור, קראס אגת
-            לקטוס וואל אאוגו וסטיבולום סוליסי טידום בעליק. קונדימנטום קורוס
-            בליקרה, נונסטי קלובר בריקנה סטום, לפריקך תצטריק לרטי.
-          </p>
-          <p>
-            Source:{" "}
-            <a href="https://en.wikipedia.org/wiki/" target="_blank">
-              Wikipedia
-            </a>
-          </p>
-        </TabPanel>
-        <TabPanel>
-          <h4>תת כותרת</h4>
-          <p>
-            לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית לורם איפסום
-            דולור סיט אמט, קונסקטורר אדיפיסינג אלית. סת אלמנקום ניסי נון ניבאה.
-            דס איאקוליס וולופטה דיאם. וסטיבולום אט דולור, קראס אגת לקטוס וואל
-            אאוגו וסטיבולום סוליסי טידום בעליק. קונדימנטום קורוס בליקרה, נונסטי
-            קלובר בריקנה סטום, לפריקך תצטריק לרטי. לורם איפסום דולור סיט אמט,
-            קונסקטורר אדיפיסינג אלית לורם איפסום דולור סיט אמט, קונסקטורר
-            אדיפיסינג אלית. סת אלמנקום ניסי נון ניבאה. דס איאקוליס וולופטה דיאם.
-            וסטיבולום אט דולור, קראס אגת לקטוס וואל אאוגו וסטיבולום סוליסי טידום
-            בעליק. קונדימנטום קורוס בליקרה, נונסטי קלובר בריקנה סטום, לפריקך
-            תצטריק לרטי.
-          </p>
-          <p>
-            Source:{" "}
-            <a href="https://en.wikipedia.org/wiki/" target="_blank">
-              Wikipedia
-            </a>
-          </p>
-        </TabPanel>
-      </Tabs>
+    <div className="bg-gray-800 w-screen h-70vh grid grid-cols-3 text-black">
+      <div className="grid bg-gray-100 justify-center items-center">
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={BCS1} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-xl">No Censorship</div>
+            <div className="text-center">Prevent data breach</div>
+          </div>
+        </div>
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={BCS2} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-center text-xl">
+              Privacy Compliance
+            </div>
+            <div className="text-center">
+              User owns data. Immutable record of activities
+            </div>
+          </div>
+        </div>
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={BCS3} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-center text-xl">Private Sharing</div>
+            <div className="text-center">Fast, secured file sharing</div>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-2 bg-gray-100 justify-self-center items-self-center">
+        <img src={BlockChainSolution} alt="block chain image" />
+      </div>
     </div>
   );
-}
+};
 
-// const CustomTab = ({ children }) => (
-//   <Tab>
-//     <h1 className="text-white">{children}</h1>
-//   </Tab>
-// );
+export const SocialMedia = () => {
+  return (
+    <div className="bg-gray-800 w-screen h-70vh grid grid-cols-3 text-black">
+      <div className="grid bg-gray-100 justify-center items-center">
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={SMS1} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-xl">Zero Liability</div>
+            <div className="text-center">
+              Prevent data breach. User owns data
+            </div>
+          </div>
+        </div>
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={SMS2} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-center text-xl">
+              Privacy Compliance
+            </div>
+            <div className="text-center">
+              User owns data. Immutable record of activities
+            </div>
+          </div>
+        </div>
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={SMS3} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-center text-xl">Enhance Brand</div>
+            <div className="text-center">
+              Achieve high quality brand leadership
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-2 bg-gray-100 justify-self-center items-self-center">
+        <img src={SocialMediaSolution} alt="block chain image" />
+      </div>
+    </div>
+  );
+};
 
-// CustomTab.tabsRole = "Tab"; // Required field to use your custom Tab
+export const Financials = () => {
+  return (
+    <div className="bg-gray-800 w-screen h-70vh grid grid-cols-3 text-black">
+      <div className="grid bg-gray-100 justify-center items-center">
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={FS1} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-xl">Prevent fraud</div>
+            <div className="text-center">Easy verification</div>
+          </div>
+        </div>
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={FS2} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-center text-xl">
+              Privacy Compliance
+            </div>
+            <div className="text-center">
+              User owns data. Immutable record of activities
+            </div>
+          </div>
+        </div>
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={FS3} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-center text-xl">
+              Easy Identity sharing
+            </div>
+            <div className="text-center">Fast, secured file sharing</div>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-2 bg-gray-100 justify-self-center items-self-center">
+        <img src={FinancialSolution} alt="block chain image" />
+      </div>
+    </div>
+  );
+};
 
-// const Solutions = () => (
-//   <div className="grid justify-center w-full ">
-//     <Tabs className="w-full">
-//       <TabList className="">
-//         <CustomTab>BlockChain</CustomTab>
-//         <CustomTab>Social Media</CustomTab>
-//         <CustomTab>Financials</CustomTab>
-//         <CustomTab>Health Care</CustomTab>
-//         <CustomTab>Supply Chain</CustomTab>
-//       </TabList>
-//       <TabPanel>
-//         <BlockChain />
-//       </TabPanel>
-//       <TabPanel>
-//         <SocialMedia />
-//       </TabPanel>
-//       <TabPanel>
-//         <Financials />
-//       </TabPanel>
-//       <TabPanel>
-//         <HealthCare />
-//       </TabPanel>
-//       <TabPanel>
-//         <SupplyChain />
-//       </TabPanel>
-//     </Tabs>
-//   </div>
-// );
+export const HealthCare = () => {
+  return (
+    <div className="bg-gray-800 w-screen h-70vh grid grid-cols-3 text-black">
+      <div className="grid bg-gray-100 justify-center items-center">
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={HCS1} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-xl">Prevent Breach</div>
+            <div className="text-center">
+              Protection against internal hackers
+            </div>
+          </div>
+        </div>
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={HCS2} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-center text-xl">
+              Privacy Compliance
+            </div>
+            <div className="text-center">
+              User owns data. Immutable record of activities
+            </div>
+          </div>
+        </div>
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={HCS3} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-center text-xl">Secured Sharing</div>
+            <div className="text-center">Fast, secured file sharing</div>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-2 bg-gray-100 justify-self-center items-self-center">
+        <img src={HealthCareSolution} alt="block chain image" />
+      </div>
+    </div>
+  );
+};
 
-// export default Solutions;
-
-// export const BlockChain = () => {
-//   return (
-//     <div className="bg-gray-800 w-screen h-70vh grid grid-cols-2">
-//       <div className="grid bg-gray-100 justify-center items-center">
-//         <div>a</div>
-//         <div>a</div>
-//         <div>a</div>
-//       </div>
-//       <div className="grid bg-gray-100 justify-center items-center">bcd</div>
-//     </div>
-//   );
-// };
-
-// export const SocialMedia = () => {
-//   return <div>Hello from the SocialMedia component.</div>;
-// };
-
-// export const Financials = () => {
-//   return <div>Hello from the Finanacials component.</div>;
-// };
-
-// export const HealthCare = () => {
-//   return <div>Hello from the HealthCare component.</div>;
-// };
-
-// export const SupplyChain = () => {
-//   return <div>Hello from the SupplyChain component.</div>;
-// };
+export const SupplyChain = () => {
+  return (
+    <div className="bg-gray-800 w-screen h-70vh grid grid-cols-3 text-black">
+      <div className="grid bg-gray-100 justify-center items-center">
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={SCS1} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-xl">Enhance brand</div>
+            <div className="text-center">
+              Achieve high quality brand leadership
+            </div>
+          </div>
+        </div>
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={SCS2} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-center text-xl">Zero liability</div>
+            <div className="text-center">Immutable records of shipments</div>
+          </div>
+        </div>
+        <div className="bg-green-100 justify-center items-center  grid grid-cols-4">
+          <div className="grid">
+            <img src={SCS3} alt="b icon" height="100px" width="100px" />
+          </div>
+          <div className="col-span-3 justify-self-center items-self-center">
+            <div className="font-bold text-center text-xl">Prevent fraud</div>
+            <div className="text-center">Easy end customer verification</div>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-2 bg-gray-100 justify-self-center items-self-center">
+        <img src={SupplyChainSolution} alt="block chain image" />
+      </div>
+    </div>
+  );
+};
